dffd85c6664cb15579f58491a323dedeb2681c8f
SanJa321 <12230041.gcit@rub.edu.bt>
2024-12-21 20:26:52 +0600

    tutorial 10 done

--- engine/features.py
+++ engine/features.py
@@ -1,3 +1,5 @@
+import sqlite3
+import webbrowser
 from playsound import playsound 
 import eel
 import os
@@ -5,6 +7,8 @@ from engine.config import ASSITANT_NAME
 from engine.command import *
 import pywhatkit as kit
 import re
+con=sqlite3.connect("jarvis.db")
+cursor=con.cursor()
 
 
 # Playing assistant sound function
@@ -18,12 +22,43 @@ def openCommand(query):
     query=query.replace("open","")
     query.lower()
 
-    if query!="":
-        speak("Opening "+query)
-        os.system("start "+query)
-    else:
-        speak("not found")
+    # if query!="":
+    #     speak("Opening "+query)
+    #     os.system("start "+query)
+    # else:
+    #     speak("not found")
+    app_name = query.strip()
 
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
+
+    
 
 def PlayYoutube(query):
     search_term=extract_yt_term(query)
